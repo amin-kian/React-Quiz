e07a47e5e6401ce0488df998484a9d990d7b427c
amin-kian <a.kianfard80@gmail.com>
2024-11-05 21:39:12 +0330

    check the anwser

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,16 @@
 import Options from "./Options";
 
-function Question({ question }) {
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
 
       <div className="options">
-        <Options question={question} />
+        <Options
+          question={question}
+          dispatch={dispatch}
+          answer={answer}
+        />
       </div>
     </div>
   );
