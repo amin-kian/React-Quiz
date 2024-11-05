a131241f5924a1fae3b71a4d05b903e5be3fd044
amin-kian <a.kianfard80@gmail.com>
2024-11-05 21:27:01 +0330

    render questions

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,13 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui">Start Quiz</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}>
+        Start Quiz
+      </button>
     </div>
   );
 }
